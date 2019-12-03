e54affea01807c18184dca9406fa2a0ac6218209
Michael Jeffrey <michaeldjeffrey@gmail.com>
2019-12-02 18:13:08 -0700

    Got the answer for the second part
    It requires some manual stuff though

--- src/Day2.elm
+++ src/Day2.elm
@@ -4,7 +4,7 @@ import Array exposing (..)
 import Browser
 import Html
 import Html.Styled as Styled exposing (..)
-import Html.Styled.Attributes exposing (css, placeholder, value)
+import Html.Styled.Attributes exposing (css, placeholder, type_, value)
 import Html.Styled.Events exposing (..)
 
 
@@ -21,7 +21,15 @@ type Msg
     | RunCommand Command
     | Step
     | StepToFinish
-    | ReplaceInitial
+    | ReplacePart1
+    | ReplacePart2
+    | Reset
+    | ReplaceInitialWith Int Int
+    | ReplaceInitialVerbNoun
+    | UpdateVerb String
+    | UpdateNoun String
+    | WithNewNoun
+    | WithNewVerb
 
 
 getCommand : Model -> Command
@@ -48,12 +56,36 @@ part1Preset =
 update : Msg -> Model -> Model
 update msg model =
     case msg of
-        ReplaceInitial ->
+        WithNewVerb ->
+            update StepToFinish (update ReplaceInitialVerbNoun (update Reset { model | verb = model.verb + 1 }))
+
+        WithNewNoun ->
+            update StepToFinish (update ReplaceInitialVerbNoun (update Reset { model | noun = model.noun + 1 }))
+
+        UpdateVerb verb ->
+            { model | verb = unsafeToInt verb }
+
+        UpdateNoun noun ->
+            { model | noun = unsafeToInt noun }
+
+        ReplaceInitialWith verb noun ->
+            { model | cleaned = fillWithPresets noun verb model.cleaned }
+
+        ReplaceInitialVerbNoun ->
+            { model | cleaned = fillWithPresets model.noun model.verb model.cleaned }
+
+        Reset ->
+            update (Input model.input) { model | cursor = 0 }
+
+        ReplacePart1 ->
             let
                 newCleaned =
                     part1Preset model.cleaned
             in
             { model | cleaned = newCleaned }
+
+        ReplacePart2 ->
+            { model | cleaned = fillWithPresets model.verb model.noun model.cleaned }
 
         Step ->
             update (RunCommand (getCommand model)) (stepCursor model)
@@ -68,7 +100,7 @@ update msg model =
             in
             case next of
                 Finished ->
-                    model
+                    { model | success = getOrZero 0 model.cleaned == model.part2Answer }
 
                 _ ->
                     update StepToFinish run
@@ -121,12 +153,32 @@ view model =
     div []
         [ h1 [] [ text "New Html Program" ]
         , h2 [] [ text model.error ]
+        , h2 []
+            [ text
+                (if model.success then
+                    "Success " ++ String.fromInt (100 * model.noun + model.verb)
+
+                 else
+                    "Not yet"
+                )
+            ]
+        , h2 [] [ text ("Distance" ++ String.fromInt (getOrZero 0 model.cleaned - model.part2Answer)) ]
         , h3 []
             [ itext model.cursor
             , text ("(" ++ String.fromInt (getOrZero (model.cursor * 4) model.cleaned) ++ ")")
             , button [ onClick Step ] [ text "Step" ]
             , button [ onClick StepToFinish ] [ text "Finish" ]
-            , button [ onClick ReplaceInitial ] [ text "Replace initial values" ]
+            , button [ onClick ReplacePart2 ] [ text "Replace Part 2 values" ]
+            , button [ onClick ReplacePart1 ] [ text "Replace part1 values" ]
+            , button [ onClick Reset ] [ text "Reset" ]
+            ]
+        , div []
+            [ button [ onClick WithNewVerb ] [ text "Go with New Verb" ]
+            , button [ onClick WithNewNoun ] [ text "Go with New Noun" ]
+            ]
+        , div []
+            [ input [ type_ "number", placeholder "verb", value (String.fromInt model.verb), onInput UpdateVerb ] []
+            , input [ type_ "number", placeholder "noun", value (String.fromInt model.noun), onInput UpdateNoun ] []
             ]
         , textarea
             [ value model.input
@@ -221,6 +273,10 @@ type alias Model =
     , commands : Array Command
     , cursor : Int
     , error : String
+    , verb : Int
+    , noun : Int
+    , part2Answer : Int
+    , success : Bool
     }
 
 
@@ -235,7 +291,16 @@ init =
     , commands = fromList <| makeCommands <| read temp
     , cursor = 0
     , error = ""
+    , verb = 0
+    , noun = 0
+    , part2Answer = 19690720
+    , success = False
     }
+
+
+unsafeToInt : String -> Int
+unsafeToInt s =
+    Maybe.withDefault 0 (String.toInt s)
 
 
 read : String -> List Int
